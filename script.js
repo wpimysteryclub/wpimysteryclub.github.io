80169299ffa3bbd635116bc9962564e612cfcf00
wpimysteryclub <131700459+wpimysteryclub@users.noreply.github.com>
2023-04-24 13:07:16 -0400

    init

---
+++ script.js
@@ -0,0 +1,109 @@
+
+// NONE OF THIS CODE IS PART OF THE GAME, PLEASE DO NOT LOOK FURTHER OR ELSE YOU WILL RUIN YOUR EXPERIENCE
+
+
+var loggedName;
+var isLoggedIn;
+const navButtons = parent.frames[0].document.getElementsByTagName("tr");
+
+startupSite();
+
+function startupSite(){
+  if(getCookie("loggedIn_WPIARG") == "true"){
+    //console.log("AHHHH");
+    siteLogin(getCookie("username_WPIARG"));
+  }
+}
+
+
+function siteLogin(name){
+  isLoggedIn = "true";
+  console.log(parent.frames[0].document.getElementById("loginButton"));
+  navButtons[2].style.visibility = "hidden";
+  navButtons[3].style.visibility = "visible";
+}
+
+function logout(){
+  if(isLoggedIn == "true"){
+    setCookie("loggedIn_WPIARG","false",1);
+    setCookie("username_WPIARG","",1);
+    navButtons[2].style.visibility = "visible";
+    navButtons[3].style.visibility = "hidden";
+  }
+}
+
+function sendLogin(){
+    var nameInp = document.getElementById("usernameInput").value;
+    var passwordInp = document.getElementById("passwordInput").value;
+    
+    validateLogin(nameInp,passwordInp);
+
+}
+
+function validateLogin(username, password){
+    if(username == "kanderson" && password == "wedgerats"){
+      confirmLogin("kyle");
+    }else if(username == "vwhite" && password == "vp"){
+      confirmLogin("veronica");
+    }else if(username == "dstar" && password =="password"){
+      confirmLogin("donna");
+    }else if(username == "jmurphy" && password == "donna"){
+      confirmLogin("jack");
+    }
+
+    
+}
+
+
+
+function setCookie(cname, cvalue, exdays) {
+    console.log("SET COOKIE " + cname + " AS " + cvalue);
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+
+    console.log(getCookie(cname));
+  }
+
+function confirmLogin(name){
+
+  switch(name){
+    case "kyle":
+      loggedName = "Kyle Anderson";
+      break;
+    case "veronica":
+      loggedName = "Veronica White";
+      break;
+    case "donna":
+      loggedName = "Donna Star";
+      break;
+    case "jack":
+      loggedName = "Jack Murphy";
+      break;
+  }
+
+  setCookie("loggedIn_WPIARG","true",6);
+  setCookie("username_WPIARG",name,6);
+  document.getElementById("loginSection").style.visibility = "hidden";
+
+  document.getElementById("welcomeMessage").innerText = "Welcome " + loggedName;
+
+  siteLogin("kyle");
+
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
